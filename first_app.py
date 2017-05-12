ca017daa7684cd310334cc67f792baf6bb7d093b
wkrt7 <wiktor7645@gmail.com>
2017-05-12 12:37:40 +0200

    gitignore

--- first_app.py
+++ first_app.py
@@ -1,21 +1,15 @@
-print('How old do you thing Fred the Chicken is?')
-number = 17
+
 
 run = True
 while run:
+    print('Choose an option')
+    print('MENU:\nq -quit\na - add\n')
+    guess = input('Enter What You Think His Age Is....t')
 
-    guess = int(input('Enter What You Think His Age Is....t'))
-
-    if guess == number:
-        print('Yes :D That is his age...')
+    if guess == 'q':
         run = False
-    elif guess < number:
-        print('No, Guess a little higher...')
-    elif guess > number:
-        print('No, Guess a little lower....')
-
-    if run == False:
-        print('Game Over')
-        choice = input('Press Q to Quit')
-        if choice == 'q':
-            break
+        print('Good bye!\n')
+    elif guess == 'a':
+        print('added\n')
+    else:
+        print('No such option - insert new character')
